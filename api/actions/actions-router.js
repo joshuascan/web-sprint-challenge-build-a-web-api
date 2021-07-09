8910370cacf6471d1b9cefa6da187b72222e47fb
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2021-07-09 11:58:31 -0700

    Build GET actions handler

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,1 +1,14 @@
-// Write your "actions" router here!
+const express = require("express");
+const Actions = require("./actions-model");
+
+const router = express.Router();
+
+router.get("/", (req, res, next) => {
+  Actions.get()
+    .then((actions) => {
+      res.status(200).json(actions);
+    })
+    .catch(next);
+});
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -2,11 +2,13 @@ const express = require("express");
 const server = express();
 
 const projectsRouter = require("./projects/projects-router");
+const actionsRouter = require("./actions/actions-router");
 const { errorHandling } = require("./generalMiddleware/index");
 
 server.use(express.json());
 
 server.use("/api/projects", projectsRouter);
+server.use("/api/actions", actionsRouter);
 
 server.use(errorHandling);
 
