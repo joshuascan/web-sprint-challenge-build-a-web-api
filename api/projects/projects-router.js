81d0ef76cce68e647789b87954d69912c202ec5f
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2021-07-09 09:47:01 -0700

    Build errorHandling middleware

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,36 @@
-// Write your "projects" router here!
+const express = require("express");
+const Projects = require("./projects-model");
+
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+  try {
+  } catch (err) {}
+});
+
+router.get("/:id", (req, res) => {
+  res.status(200).json(req.method);
+  console.log(req.method);
+});
+
+router.post("/", (req, res) => {
+  res.status(200).json(req.method);
+  console.log(req.method);
+});
+
+router.put("/:id", (req, res) => {
+  res.status(200).json(req.method);
+  console.log(req.method);
+});
+
+router.delete("/:id", (req, res) => {
+  res.status(200).json(req.method);
+  console.log(req.method);
+});
+
+router.get("/:id/actions", (req, res) => {
+  res.status(200).json(req.method);
+  console.log(req.method);
+});
+
+module.exports = router;
